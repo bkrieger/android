d42bc531dab3a6f1575031b5f242826fd7c41219
Alex Yau <ayau@sas.upenn.edu>
2013-09-09 23:32:57 -0400

    styling of avatar

--- src/us/happ/android/utils/Media.java
+++ src/us/happ/android/utils/Media.java
@@ -29,16 +29,14 @@ public class Media {
 	public static Bitmap getRoundedCornerBitmap(Context context, Bitmap bitmap, float decay, long phoneNumber) {
 		boolean noProfile = bitmap == null;
 		
-		Bitmap output;
+		Bitmap output = Bitmap.createBitmap((int) pxFromDp(context, 70f), (int) pxFromDp(context, 70f), Config.ARGB_8888);
 		
-		int profileColor = 0;
+		int profileColor = generateColor(phoneNumber);
+		
+		int bitmapWidth = (int) pxFromDp(context, 60f);
 		
 		if (noProfile) {
 			bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.ic_avatar);
-			output = Bitmap.createBitmap((int) pxFromDp(context, 80f), (int) pxFromDp(context, 80f), Config.ARGB_8888);
-			profileColor = generateColor(phoneNumber);
-		}else {
-			output = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), Config.ARGB_8888);
 		}
 		
 		Canvas canvas = new Canvas(output);
@@ -47,68 +45,52 @@ public class Media {
 
 	    final int color = 0xff424242;
 	    final Paint paint = new Paint();
-	    Rect rect;
-	    RectF rectF;
-	    final Rect bitmapRect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
-	    int padding = (int) pxFromDp(context, 5f);
 	    
-	    if (noProfile){
-	    	int bitmapWidth = (int) pxFromDp(context, 60f);
-	    	
-	    	rect = new Rect(padding, padding, bitmap.getWidth() + padding, bitmap.getHeight() + padding);
-	    	rectF = new RectF(rect);
-	    	
-	    	padding = bitmap.getWidth() - bitmapWidth;
-	    	rect = new Rect(padding, padding, bitmapWidth + padding, bitmapWidth + padding);
-	    } else {
-	    	rect = bitmapRect;
-	    	rectF = new RectF(bitmapRect);
-	    }
+	    Rect bitmapRect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
+	    Rect rect = new Rect(borderPx, borderPx, bitmapWidth + borderPx, bitmapWidth + borderPx);
+	    RectF rectF = new RectF(rect);
 	    
-	    final float roundPx = pxFromDp(context, 80);
+	    final float roundPx = pxFromDp(context, 70f);
 
 	    paint.setAntiAlias(true);
 	    canvas.drawARGB(0, 0, 0, 0);
 	    
-	    if (noProfile){
-	    	paint.setColor(profileColor);
-	    	canvas.drawRoundRect(rectF, roundPx, roundPx, paint);
-	    }
+    	paint.setColor(profileColor);
+    	canvas.drawRoundRect(rectF, roundPx, roundPx, paint);
 	    
 	    paint.setColor(color);
 	    
 	    if (!noProfile){
-		    canvas.drawRoundRect(rectF, roundPx, roundPx, paint);
 		    paint.setXfermode(new PorterDuffXfermode(Mode.SRC_IN));
 	    }
 		
-	    canvas.drawBitmap(bitmap, bitmapRect, rect, paint);
+	    canvas.drawBitmap(bitmap, bitmapRect, rect, paint);  
+	    paint.setXfermode(null);
 	    
-	    
-//	    // draw border
-	    if (noProfile){
-	    	rect = new Rect(borderPx/2, borderPx/2, bitmap.getWidth() + borderPx*3/2, bitmap.getHeight() + borderPx*3/2);
-	    	rectF = new RectF(rect);
-	    	paint.setColor(profileColor);
-	    	paint.setAlpha(50);
-	    } else {
-	    	paint.setColor(context.getResources().getColor(R.color.light_purple));
-	    }
+	    // draw border
+    	rect = new Rect(borderPx/2, borderPx/2, bitmapWidth + borderPx*3/2, bitmapWidth + borderPx*3/2);
+    	rectF = new RectF(rect);
+    	paint.setColor(profileColor);
+    	paint.setAlpha(50);
+
 	    paint.setStyle(Paint.Style.STROKE);
 	    paint.setStrokeWidth((float) borderPx);
 	    canvas.drawRoundRect(rectF, roundPx, roundPx, paint);
 	    
-	    // draw time left	   
+	    int onedp;
+	    // draw time left
 	    if (noProfile){
-	    	rect = new Rect(borderPx, borderPx, bitmap.getWidth() + borderPx, bitmap.getHeight() + borderPx);
-	    	rectF = new RectF(rect);
-	    	paint.setColor(profileColor);
-		    paint.setAlpha(255);
-		    paint.setStrokeWidth((float) borderPx*3/2 + pxFromDp(context, 3));
+	    	onedp = (int) pxFromDp(context, 1);
 	    } else {
-	    	paint.setColor(context.getResources().getColor(R.color.happ_purple));
-		    paint.setAlpha(255);
+	    	onedp = 0;
 	    }
+	    
+	    rect = new Rect(borderPx/2 + onedp, borderPx/2 + onedp, bitmapWidth + borderPx*3/2 - onedp, bitmapWidth + borderPx*3/2 - onedp);
+    	rectF = new RectF(rect);
+    	paint.setColor(profileColor);
+	    paint.setAlpha(255);
+	    paint.setStrokeWidth((float) borderPx + onedp);
+
 	    int start = (int) (270 - decay*360);
 	    int end = (int) (decay*360);
 	    canvas.drawArc(rectF, start, end, false, paint);
